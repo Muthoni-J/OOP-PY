f0b74d5bf537151d26ad18d112d196543034202a
Muthoni-J <jmjoanmuthoni@gmail.com>
2022-06-20 22:32:09 +0300

    changes

--- Deposit.py
+++ Deposit.py
@@ -6,6 +6,39 @@
 # Add a new method called withdrawals_statement which prints each withdrawal in a new line
 # Modify the withdrawal method to include a transaction fee of 100 per transaction.
 # Add a method to show the current balance
+# Update the withdrawal method to store each withdrawal transaction as a dictionary like like this 
+# {
+#    "date": datetime object,
+#    "amount": amount,
+#    "narration": deposit or withdrawal
+# }
+
+# Update the deposit method to store each deposit transaction as a dictionary like this 
+# {
+#    "date": datetime object,
+#    "amount": amount,
+#    "narration": deposit or withdrawal
+# }
+
+# Add a new method  full_statement which combines both deposits and withdrawals into one list ordered by date and using a for loop prints each transaction in a new line like this
+# 16/06/22 —----- Deposit —---- 1000
+
+# Add a new attribute loan_balance which is zero by default.
+
+# Add a borrow method which allows a customer to borrow if they meet these conditions:
+# Customer has made at least 10 deposits.
+# Loan amount requested must be more than 100
+# A customer qualifies for a loan amount that is less than  or equal to 1/3 of their total sum of deposit history
+# Customer account has no has no balance
+# Customer has no outstanding loan
+# The loan attracts  an interest of 3%.
+
+# Add a loan repayment method with these conditions
+# A customer can repay a loan to reduce the current loan balance
+# Overpayment of a loan increases a customers current deposit
+
+# Add a new method transfer which accepts two attributes (amount and instance of another account). If the amount is less than the current instances balance, the method transfers the requested amount from the current account to the passed account. The transfer is accomplished by reducing the current account balance and depositing the requested amount to the passed account.
+
 
 
 from datetime import datetime
@@ -23,6 +56,7 @@ class Account:
         self.statement=[]
         self.time=datetime.now().strftime("%X")
         self.loan_balance=0
+        
        
  
     def deposit(self,amount):
@@ -62,7 +96,7 @@ class Account:
         print(f"Your deposits{n}")
     
     def widthdals_statement(self):     
-       for n in self.widhdrawals:
+       for n in self.withdraws:
         print(f"Your Widthdrawals{n}")
      
     def current_balance(self):
@@ -82,7 +116,7 @@ class Account:
        interest = amount*0.03
        if amount<=100:
            return "Loan must be more than 100"
-       elif self.loan>0:
+       elif self.loan_balance>0:
            return "Loan denied, kindly repay your current loan of {self.loan}"
        elif len(self.deposits)<10:
            return f"you deposits must be at least more than 10"
@@ -116,13 +150,13 @@ class Account:
                "amount": amount,
                "narration": "You have withdrawn"
                }
-           self.withdrawal_transaction.append(transaction)
-           return f"Dear customer {self.name} you have fully paid your loan and the overpay of {payment} is added on your account, your new balance is {self.balance}"
+           self.statement.append(transaction)
+           return f"Dear customer {self.acc_name} you have fully paid your loan and the overpay of {payment} is added on your account, your new balance is {self.balance}"
        
     def transfer(self, account_instance,amount):
-       total = amount + self.withdrawal
+       total = amount + self.withdraws
        if amount<0:
-           return f"Dear customer {self.name} your amount is too low"
+           return f"Dear customer {self.acc_name} your amount is too low"
        elif total>self.balance:
            return f"Your balance is {self.balance} and you need atleast {total}"
        else:
